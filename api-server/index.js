b979d6c4457f089a7c5d131dc271ea23aefaebe5
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-10-15 17:14:15 +0530

    bug: send json to frontend

--- api-server/index.js
+++ api-server/index.js
@@ -135,7 +135,7 @@ const init = async () => {
 
                     const existingChannel = redisChannels.get(socket)
                     if (existingChannel) {
-                        socket.send(`Unsubscribing from channel : ${existingChannel}`)
+                        socket.send(JSON.stringify({ status: 'unsubscribed', channel: existingChannel }));
                         await subscriber.unsubscribe(existingChannel)
                     }
 
