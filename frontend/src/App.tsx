aaa2d0ece0b7f0ee01324310032b6fbd6f842567
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-10-23 15:33:58 +0530

    chore: change header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -118,7 +118,7 @@ function App() {
     return (
         <div className="app">
             <div className="header">
-                <h1 style={{ color: 'white', fontSize: '25px' }}>Verceless by <a style={{ textDecoration: 'none' }} href="https://x.com/intent/follow?screen_name=harsh_twtt" target='_blank' rel='noopener'>@harsh_twtt</a></h1>
+                <h1 style={{ color: 'white', fontSize: '25px' }}>VerceLESS by <a style={{ textDecoration: 'none' }} href="https://x.com/intent/follow?screen_name=harsh_twtt" target='_blank' rel='noopener'>@harsh_twtt</a></h1>
             </div>
 
             <div className="main-container">
